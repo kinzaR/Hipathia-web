fae64a1422980518e29d6e062b748ee18dd10403
Kinza Rian <rian.kinza@gmail.com>
2018-03-06 19:23:34 +0100

    Add files via upload

---
+++ conf/config.js
@@ -0,0 +1,16 @@
+﻿/**
+ * This is a configuration file.
+ * Changes to this file may cause the application does not work as it should
+ **/
+// STEVIA_MANAGER_LOG = true;
+//STEVIA_SERVER_HOST = '//ws.babelomics.org/stv01';
+STEVIA_SERVER_HOST = 'http://ws2.babelomics.org/stevia_cellmaps_hipathia_pathact_metabolizer';
+//STEVIA_SERVER_HOST = '//ws2.babelomics.org/stevia_cellmaps_hipathia_pathact_metabolizer';
+// STEVIA_SERVER_HOST = '//ws.babelomics.org/teststv01';
+// STEVIA_SERVER_HOST = '//localhost:5555';
+
+// STEVIA_SHOW_JOB_ERROR_FILES = false;
+STEVIA_SHOW_JOB_ERROR_FILES = true;
+UPLOAD_FILES_WITH_ERRORS = true;
+// STEVIA_SHOW_HIDDEN_FILES = false;
+STEVIA_SHOW_HIDDEN_FILES = true;
